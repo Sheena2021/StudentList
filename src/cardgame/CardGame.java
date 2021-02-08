bd8fb3b633e3ad39ede2960ac5d2aacb4a0e81b1
Sheena Manuel <manuels@sheridancollege.com>
2021-02-07 20:46:43 -0500

    After refactoring using enums (loosely coupled)

--- src/cardgame/CardGame.java
+++ src/cardgame/CardGame.java
@@ -1,5 +1,6 @@
 package cardgame;
-
+import cardgame.Card.Value;
+import cardgame.Card.Suit;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -14,48 +15,57 @@ public class CardGame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-           Scanner input = new Scanner(System.in);
+            Scanner input = new Scanner(System.in);
 
-        // Create an array to hold 7 cards
-//        Card[] hand = new Card[7];
+            // Create an array to hold 7 cards
+            Card[] hand = new Card[7];
+            
+            Value[] cardValues = Value.values();            
+            Suit[] cardSuits = Suit.values();
+            
 
-        // We'll use Random to generate random numbers
-        Random random = new Random();
+            // We'll use Random to generate random numbers
+            Random random = new Random();
 
-//        for (int i = 0; i < hand.length; i++) {
-//            int value = random.nextInt(13) + 1;
-//            String suit = Card.SUITS[random.nextInt(4)];
-//
-//            Card card = new Card(value, suit);
-//            hand[i] = card;
-//        }
+            for (int i = 0; i < hand.length; i++) {
+                //get random value
+                Value value = cardValues[random.nextInt(cardValues.length)];
+                Suit suit = cardSuits[random.nextInt(cardSuits.length)];
 
-        // print them out for debugging purposes
-        System.out.println("Here are the cards in the hand");
-//        for (Card card : hand) {
-//            System.out.printf("%d of %s\n", card.getValue(), card.getSuit());
-//        }
+                Card card = new Card(value, suit);
+                hand[i] = card;
+      }
 
-        // Now ask the user for a card
-        System.out.println("Pick a suit for your card");
-//        for (int i = 0; i < Card.SUITS.length; i++) {
-//            System.out.println((i + 1) + ": " + Card.SUITS[i]);
-//        }
-        int suit = input.nextInt();
+            // print them out for debugging purposes
+            System.out.println("Here are the cards in the hand");
+            for (Card card : hand) {
+            System.out.printf("%s of %s\n", card.getValue(), card.getSuit());
+        }
 
-        System.out.println("Enter a value (1 to 13)");
-        int value = input.nextInt();
+            // Now ask the user for a card
+            System.out.println("Pick a suit for your card");
+            for (int i = 0; i < cardSuits.length; i++) {
+                System.out.println((i + 1) + ": " + cardSuits[i]);
+            }
+            int suitPosition = input.nextInt() - 1;
 
-//        Card userGuess = new Card(value, Card.SUITS[suit - 1]);
+            System.out.println("Enter a value)");
+            for (int i = 0; i < cardValues.length; i++) {
+                System.out.println((i + 1) + ": " + cardValues[i]);
+            }
+            
+            int valuePosition = input.nextInt() - 1;
 
-        boolean match = false;
-//        for (Card card : hand) {
-//            if (card.getValue() == userGuess.getValue()
-//                    && card.getSuit().equals(userGuess.getSuit())) {
-//                match = true;
-//                break;
-//            }
-//        }
+            Card userGuess = new Card(cardValues[valuePosition], cardSuits[suitPosition]);
+
+            boolean match = false;
+            for (Card card : hand) {
+                if (card.getValue() == userGuess.getValue()
+                    && card.getSuit() == userGuess.getSuit()) {
+                match = true;
+                break;
+            }
+        }
     
         String response = match ? "Right guess": "No match";
         
